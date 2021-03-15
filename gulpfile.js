b635be3c9a17c9d1673721181de95607c55fd5ae
Max-Mashkov <vfirjdvfrcbv@gmail.com>
2021-03-15 04:18:53 +0200

    Added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let project_folder = "dist";
 let source_folder = "#src";
 
+let fs = require('fs');
+
 let path = {
     build : {
         html:project_folder + "/",
@@ -27,20 +29,23 @@ let path = {
 
 let { src, dest } = require('gulp'),
     gulp = require('gulp'),
-    browsersync = require("browser-sync").create();
-    fileinclude = require ("gulp-file-include");
-    del = require ("del");
-    scss = require ("gulp-sass");
-    autoprefixer = require ("gulp-autoprefixer");
-    group_media = require ("gulp-group-css-media-queries");
-    clean_css = require ("gulp-clean-css");
-    rename = require ("gulp-rename");
-    uglify = require ("gulp-uglify-es").default;
-    imagemin = require ("gulp-imagemin");
-    webp = require ("gulp-webp");
-    webphtml = require ("gulp-webp-html");
-    webpcss = require ("gulp-webp-css");
-    svgSprite = require ("gulp-svg-sprite");
+    browsersync = require("browser-sync").create(),
+    fileinclude = require ("gulp-file-include"),
+    del = require ("del"),
+    scss = require ("gulp-sass"),
+    autoprefixer = require ("gulp-autoprefixer"),
+    group_media = require ("gulp-group-css-media-queries"),
+    clean_css = require ("gulp-clean-css"),
+    rename = require ("gulp-rename"),
+    uglify = require ("gulp-uglify-es").default,
+    imagemin = require ("gulp-imagemin"),
+    webp = require ("gulp-webp"),
+    webphtml = require ("gulp-webp-html"),
+    webpcss = require ("gulp-webp-css"),
+    svgSprite = require ("gulp-svg-sprite"),
+    ttf2woff = require ("gulp-ttf2woff"),
+    ttf2woff2 = require ("gulp-ttf2woff2"),
+    fonter = require ("gulp-fonter");
 
 function browserSync(params) {
     browsersync.init({
@@ -126,6 +131,23 @@ function images () {
         .pipe(browsersync.stream())
 }
 
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+};
+
+gulp.task('otf2ttf', function (){
+    return src([source_folder + '/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(source_folder + '/fonts/'));
+})
+
 gulp.task('svgSprite', function (){
     return gulp.src([source_folder + '/iconsprite/*.svg'])
         .pipe(svgSprite({
@@ -140,6 +162,32 @@ gulp.task('svgSprite', function (){
         .pipe(dest(path.build.img))
 })
 
+function fontsStyle(params) {
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+
+
+function cb() {
+
+}
+
 function watchFiles(params) {
     gulp.watch([path.watch.html],html);
     gulp.watch([path.watch.css],css);
@@ -151,9 +199,11 @@ function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
